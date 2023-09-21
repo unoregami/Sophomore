c929b7708ee34867bee6f4b85884f89c665a7ed2
uno <137420541+unoregami@users.noreply.github.com>
2023-09-21 08:55:43 +0800

    Update 2D.py

--- activity/2D.py
+++ activity/2D.py
@@ -1,15 +1,22 @@
+def printArray(array):
+	i = 0
+	j = 0
+	while (i < 2):
+		print(f"| {array[i][j]} ", end="")
+	
+		j += 1
+		if j < 3:
+			continue
+		else:
+			i += 1
+			j = 0
+			print("\n")
+
+
 fruits = [["banana", "apple", "orange"], ["cherry", "kiwi", "peach"]]
 
-i = 0
-j = 0
+printArray(fruits)
 
-while (i < 2):
-	print(f"| {fruits[i][j]} ", end="")
-	
-	j += 1
-	if j < 3:
-		continue
-	else:
-		i += 1
-		j = 0
-		print("\n")
+print("--------------------------------------------------------")
+fruits[1][2] = "guava"
+printArray(fruits)
